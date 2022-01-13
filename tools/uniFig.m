36c7075f003698c2bece72a7a13094ee99a9284d
Lukas Pinsker <lukas.pinsker@gmail.com>
2022-01-13 15:00:47 +0100

    added support for better autoranging

--- tools/uniFig.m
+++ tools/uniFig.m
@@ -2,6 +2,9 @@ function uniFig(fig_path)
 %UNIFIG adapts figure for papers
 %   recolors the figure to suit acadamic papers
 
+% issues:
+% - cannot detect discrete lines
+
 %% CONSTANTS
 
 % base colors
@@ -12,17 +15,50 @@ c_lightgrey = [0.5, 0.5, 0.5];
 % colors for lines
 c_red =     [1, 0, 0];
 c_orange =  [1.00, 0.4, 0.16];
-c_blue =    [0.07, 0.62, 1.00];
+c_blue =    [0.00, 0.45, 0.74];
 c_green =   [0.39, 0.83, 0.07];
 c_violet =  [0.72, 0.27, 1.00];
 
 % line color scheme
 c_line = {c_blue, c_red, c_green, c_violet, c_orange};
 
+XMode = "auto";
+YMode = "manual";
+ZMode = "manual";
+
 %% variables
 color = 1;
+XLim = [inf, -inf];
+YLim = [inf, -inf];
+ZLim = [inf, -inf];
+
+% oversize range of plot in percent
+XOversize = 0.001;
+YOversize = 0.10;
+ZOversize = 0.05;
 
 %% subfunctions
+function setMinMax(object, axis)
+
+    % min value
+    Min = eval("min(object." + axis + "Data);");
+
+    % max value
+    Max = eval("max(object." + axis + "Data);");
+
+    % store min
+    if ~isempty(Min)
+        eval(axis + "Lim(1) = min(" + axis + "Lim(1), Min);");
+    end
+
+    % store max
+    if ~isempty(Max)
+        eval(axis + "Lim(2) = max(" + axis + "Lim(2), Max);");
+    end
+
+
+end
+
 function axes(object)
 
     object.Color = c_white;
@@ -48,6 +84,11 @@ function line(object)
 
     % select next color, avoid indexing out range
     color = mod(color,length(c_line)) + 1;
+
+    % store range of line
+    setMinMax(object,"X");
+    setMinMax(object,"Y");
+    setMinMax(object,"Z");
 
 end
 
@@ -76,6 +117,11 @@ function inspect_child(object)
 
         % reset colormap
         color = 1;
+
+        % reset scaling
+        XLim = [inf, -inf];
+        YLim = [inf, -inf];
+        ZLim = [inf, -inf];
      
         axes(object);
 
@@ -101,10 +147,39 @@ function inspect_child(object)
     % recursive call
     if object.isprop("Children")
         child = object.Children;
-        
+
         for i = 1:length(child)
             inspect_child(child(i));
+        end    
+    end
+
+    if strcmp(obj_class, 'matlab.graphics.axis.Axes')
+        % set limit for axis
+        dX = XLim(2) - XLim(1);
+        dY = YLim(2) - YLim(1);
+        dZ = ZLim(2) - ZLim(1);
+
+        % calculate delta size
+        dX = dX * XOversize;
+        dY = dY * YOversize;
+        dZ = dZ * ZOversize;
+
+        % set range
+        if abs(dX) ~= inf
+            object.XLim = [XLim(1) - dX, XLim(2) + dX];
         end
+        if abs(dY) ~= inf
+            object.YLim = [YLim(1) - dY, YLim(2) + dY];
+        end
+        if abs(dZ) ~= inf
+            object.ZLim = [ZLim(1) - dZ, ZLim(2) + dZ];
+        end
+
+        % set range
+        object.XLimMode = XMode;
+        object.YLimMode = YMode;
+        object.ZLimMode = ZMode;
+
     end
 
 end
