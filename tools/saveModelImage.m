078c4545d0bf10cd6c2a8ead2d4f860c02ae0090
Lukas Pinsker <lukas.pinsker@gmail.com>
2022-01-27 17:06:39 +0100

    open models stay open

--- tools/saveModelImage.m
+++ tools/saveModelImage.m
@@ -1,7 +1,6 @@
 function saveModelImage(model_path, image_path, subsystem)
 %saveModelImage prints simulink models as vectorgraphic
 
-
 % varargin
 if ~exist("image_path")
     image_path = model_path;
@@ -13,12 +12,17 @@ if ~exist("subsystem")
 end
 
 % open model
+isopen = bdIsLoaded(model_path);        % if model is open dont close it
 open_system(model_path, 'loadonly');
 
 % print model
+% model_handle = get_param(model_path + subsystem , 'Handle');
+% saveas(model_handle, image_path, 'pdf');
 print("-s" + model_path + subsystem, "-vector", "-dpdf", image_path);
 
 % close model
-close_system(model_path);
+if ~isopen
+    close_system(model_path);
+end
 
 end
